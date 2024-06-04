5253fd797d52e9fa712517cb8408b4a83fcdf923
mangasamudram_sruthi <122959311+mangasamudram-sruthi@users.noreply.github.com>
2024-06-04 23:31:36 +0530

    Add files via upload

---
+++ Basic_python/Assignment/monotonic array.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def is_monotonic(nums):
+    increasing = True
+    decreasing = True
+    
+    for i in range(1, len(nums)):
+        if nums[i] < nums[i - 1]:
+            increasing = False
+        if nums[i] > nums[i - 1]:
+            decreasing = False
+    
+    return increasing or decreasing
+
+
+nums1 = [1, 2, 2, 3]
+print(is_monotonic(nums1)) 
+nums2 = [6, 5, 4, 4]
+print(is_monotonic(nums2))  
+
+nums3 = [1, 3, 2]
+print(is_monotonic(nums3))  
+
+
+# In[ ]:
+
+
+
+
---
+++ Basic_python/Assignment/roman number to integer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+def roman_to_int(s):
+    
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}   
+    # Initialize the result
+    result = 0
+    for i in range(len(s)):
+        current_value = roman_map[s[i]]
+        if i + 1 < len(s) and roman_map[s[i + 1]] > current_value:
+            result -= current_value
+        else:
+            result += current_value
+    
+    return result
+s1 = "III"
+print(roman_to_int(s1))
+
+s2 = "LVIII"
+print(roman_to_int(s2))  
+
+s3 = "MCMXCIV"
+print(roman_to_int(s3))  
+
+
+# In[ ]:
+
+
+
+
