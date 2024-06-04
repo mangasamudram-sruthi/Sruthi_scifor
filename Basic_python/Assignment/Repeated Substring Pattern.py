b9358e14687c44d70117d69fd9637cdbc1387d87
mangasamudram_sruthi <122959311+mangasamudram-sruthi@users.noreply.github.com>
2024-06-04 23:05:56 +0530

    Add files via upload

---
+++ Basic_python/Assignment/Repeated Substring Pattern.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def repeated_substring_pattern(s):
+    n = len(s)    
+    # Check each possible length of the substring
+    for i in range(1, n // 2 + 1):
+        # If i is a divisor of n
+        if n % i == 0:
+            # Check if the substring s[:i] can be repeated to form s
+            repeated = s[:i] * (n // i)
+            if repeated == s:
+                return True   
+    return False
+s1 = "abab"
+print(repeated_substring_pattern(s1))  
+s2 = "aba"
+print(repeated_substring_pattern(s2))  
+s3 = "abcabcabcabc"
+print(repeated_substring_pattern(s3))  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
