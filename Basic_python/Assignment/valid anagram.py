4ab18e4fd6c350fbebfc0dddb26c3539f9f98438
mangasamudram_sruthi <122959311+mangasamudram-sruthi@users.noreply.github.com>
2024-06-04 23:01:41 +0530

    Add files via upload

---
+++ Basic_python/Assignment/valid anagram.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+def is_anagram(s, t):
+    # Check if the lengths are equal
+    if len(s) != len(t):
+        return False
+
+    s_counts = Counter(s)
+
+    for char in t:
+        if char in s_counts:
+            s_counts[char] -= 1
+        else:
+            return False
+    
+    # Check if all counts are zero
+    for count in s_counts.values():
+        if count != 0:
+            return False
+    
+    return True
+
+s1 = "anagram"
+t1 = "nagaram"
+print(is_anagram(s1, t1)) 
+
+s2 = "rat"
+t2 = "car"
+print(is_anagram(s2, t2)) 
+
+
+# In[ ]:
+
+
+
+
