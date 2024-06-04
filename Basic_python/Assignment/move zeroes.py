a78fa0ff14be9479dc5bf0f5c21c475b79effc89
mangasamudram_sruthi <122959311+mangasamudram-sruthi@users.noreply.github.com>
2024-06-04 23:12:06 +0530

    Add files via upload

---
+++ Basic_python/Assignment/move zeroes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def move_zeros(nums):
+    slow = 0
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow] = nums[fast]
+            slow += 1
+    
+    # Fill the remaining part with zeros
+    while slow < len(nums):
+        nums[slow] = 0
+        slow += 1
+
+
+nums1 = [0, 1, 0, 3, 12]
+move_zeros(nums1)
+print(nums1) 
+
+nums2 = [0]
+move_zeros(nums2)
+print(nums2)  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
