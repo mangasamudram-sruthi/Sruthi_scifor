ec99c8c7a40d2d51c2466a7aaa25a8b7a8d46ac3
mangasamudram_sruthi <122959311+mangasamudram-sruthi@users.noreply.github.com>
2024-06-10 20:56:53 +0530

    Add files via upload

---
+++ Basic_python/Assignment-2/Strings.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1)String Concatenation
+
+# In[10]:
+
+
+a = "Hello"
+b = "Sruthi"
+message = a + ", " + b
+print(message)
+
+
+# # 2)Reversing String
+
+# In[11]:
+
+
+Msg = "Hello  World"
+
+b = Msg[::-1]
+print(b)
+
+
+# # 3)Basic Operations
+
+# In[14]:
+
+
+text = "Hello, World!"
+length = len(text)  
+substring = text[0:5] 
+upper_text = text.upper()  
+new = text.replace("World", "Python") 
+print(length)
+print(substring)
+print(upper_text)
+print(new)
+
+
+# # 4)Title Case conversion
+# 
+
+# In[17]:
+
+
+a= "hello world, welcome to python programming"
+title = a.title()  
+print(title)
+ 
+
+
+# # 5)Number of occurences
+
+# In[19]:
+
+
+s = "banana"
+substring = "a"
+count = s.count(substring)  
+print(count)
+
+
+# In[ ]:
+
+
+
+
